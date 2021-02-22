b4d9f70182c3f61a9cf8e4ccbf4e41f6d1ccfe7d
Dripcoding <dripcoding@gmail.com>
2021-02-21 18:43:30 -0800

    implement deliver methods in concrete product classes

--- FactoryMethod/index.ts
+++ FactoryMethod/index.ts
@@ -11,6 +11,11 @@ class LogisticsApp {
 
     // superclass's responsibility is to use concrete products (ie. business logic)
     // code for product creation should be separate from this class
+    public someOperation():void {
+        const product = this.createTransport();
+        console.log("Creator's code creates the default transport\n");
+        console.log(product.deliver());
+    }
 
     public createTransport(): ITransport {
         return new Car();
@@ -43,21 +48,29 @@ class PlaneLogistics extends LogisticsApp {
 // these are objects returned by factory method
 class Truck implements ITransport {
 
-    deliver() {}
+    deliver() {
+        console.log("Truck delivers something")
+    }
 }
 
 class Ship implements ITransport {
-
-    deliver() {}
+    
+    deliver() {
+        console.log("Ship delivers something")
+    }
 }
 
 class Plane implements ITransport {
-
-    deliver() {}
+    
+    deliver() {
+        console.log("Plane delivers something")
+    }
 }
 
 // default product returned in the superclass's factory method
 class Car implements ITransport {
 
-    deliver() {}
+    deliver() {
+        console.log("Car delivers something")
+    }
 }
