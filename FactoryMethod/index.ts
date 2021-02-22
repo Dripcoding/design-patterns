df0de764900ab688a90fdf7c049042a0937eb66f
Dripcoding <dripcoding@gmail.com>
2021-02-21 18:37:07 -0800

    override factory method in concrete creator classes

--- FactoryMethod/index.ts
+++ FactoryMethod/index.ts
@@ -9,17 +9,35 @@ interface ITransport {
 // defines a factory method which separates object creation logic from business logic
 class LogisticsApp {
 
+    // superclass's responsibility is to use concrete products (ie. business logic)
+    // code for product creation should be separate from this class
+
     public createTransport(): ITransport {
         return new Car();
     }
 }
 
 // concrete creators that are responsible for overriding the superclass's factory method
-class TruckLogistics extends LogisticsApp {}
+class TruckLogistics extends LogisticsApp {
 
-class ShipLogistics extends LogisticsApp {}
+    public createTransport(): ITransport {
+        return new Truck();
+    }
+}
 
-class PlaneLogistics extends LogisticsApp {}
+class ShipLogistics extends LogisticsApp {
+
+    public createTransport(): ITransport {
+        return new Ship();
+    }
+}
+
+class PlaneLogistics extends LogisticsApp {
+
+    public createTransport(): ITransport {
+        return new Plane();
+    }
+}
 
 // concrete products are implementations of the common interface
 // these are objects returned by factory method
